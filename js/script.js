03b8ddff5635cb8c0b7090d9e80626200d3fb6b2
Ryukikun <ridhodzaki03@gmail.com>
2021-09-07 08:08:26 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// $(window).on('load', function() {
+//   // console.log('oke');
+//   $('.pKiri').addClass('pmuncul');
+//   $('.pKanan').addClass('pmuncul');
+// });
+const myText = document.querySelector(".typed-text");
+const cursor = document.querySelector(".cursor");
+
+const textArray = ["Semoga dihari yang spesial ini, jadi hari yang spesial buat kamu Dan selalu di beri umur yang panjang dari yang maha kuasa. Sehat terus yaa!!! ammiiin .... semoga di tahun ini menjadi pribadi yang lebih baik dari sebelum nya Semoga kuliah nya selalu lancar."];
+const typingDelay = 150;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursor.classList.contains("typing")) cursor.classList.add("typing")
+    myText.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    if (textArray[textArrayIndex].charAt(charIndex) === "." || textArray[textArrayIndex].charAt(charIndex) === ",") {
+      setTimeout(type, 500);
+    } else {
+      setTimeout(type, typingDelay);
+    }
+  } else {
+    cursor.classList.remove("typing")
+    $('.container.kanan').addClass('muncul')
+  }
+} 
+
+var mySong = document.getElementById("mySong");
+var icon = document.getElementById("icon");
+var txtArea = document.getElementById("salamHangat") ;
+
+icon.onclick = function() {
+  $('.container.hide').addClass('muncul')
+  if (mySong.paused) {
+    mySong.play()
+  }
+  setTimeout(type, 3000)
+  type()
+}
