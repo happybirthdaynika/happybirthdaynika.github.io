38310ffdbcb40bc82c18fc3f7ad1dd3a88695d76
Ryukikun <ridhodzaki03@gmail.com>
2021-09-07 10:04:10 +0700

    update fix

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
-// $(window).on('load', function() {
-//   // console.log('oke');
-//   $('.pKiri').addClass('pmuncul');
-//   $('.pKanan').addClass('pmuncul');
-// });
+var kedua = 211;
+var ketiga = 268;
+let hScroll;
+let charIndex = 0;
+
+$(window).on('load', function() {
+  // console.log('oke');
+  $('.pKanan').addClass('pmuncul');
+});
+
+$(window).scroll(function() { 
+  hScroll = $(this).scrollTop();
+  console.log(hScroll);
+  if (hScroll >= kedua && charIndex >= 227) {
+    console.log('jalan')
+    $('.container.kedua').addClass('muncul')
+  }
+  if (hScroll >= ketiga && charIndex >= 227) {
+    console.log('jalan ketiga')
+    $('.container.ketiga').addClass('muncul')
+  }
+})
+
 const myText = document.querySelector(".typed-text");
 const cursor = document.querySelector(".cursor");
 
-const textArray = ["Semoga dihari yang spesial ini, jadi hari yang spesial buat kamu Dan selalu di beri umur yang panjang dari yang maha kuasa. Sehat terus yaa!!! ammiiin .... semoga di tahun ini menjadi pribadi yang lebih baik dari sebelum nya Semoga kuliah nya selalu lancar."];
+const textArray = ["Semoga dihari yang spesial ini, jadi hari yang spesial buat kamu Dan selalu di beri umur yang panjang dari yang maha kuasa. Sehat terus yaa!!! ammiiin .... semoga di tahun ini, menjadi pribadi yang lebih baik dari sebelum nya.."];
 const typingDelay = 150;
 const newTextDelay = 2000;
 let textArrayIndex = 0;
-let charIndex = 0;
 
 function type() {
   if (charIndex < textArray[textArrayIndex].length) {
@@ -26,7 +43,7 @@ function type() {
     cursor.classList.remove("typing")
     $('.container.kanan').addClass('muncul')
   }
-} 
+}
 
 var mySong = document.getElementById("mySong");
 var icon = document.getElementById("icon");
